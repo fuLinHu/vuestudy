855506256b33600d3a4ebafdda5080ba421376aa
linhu <1205668006@qq.com>
2020-07-09 09:02:32 +0800

    首次提交

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="login-container">
+		<div class='login'>
+			
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped="scoped">  /* scoped 当前页面生效 */
+   .login-container{
+	   background-color: blueviolet;
+	   height: 100%;
+   }
+   .login{
+	   width: 450px;
+	   height: 300px;
+	   background-color: white;
+	   border-radius: 3px;
+	  /* 据中得css */
+	   position: absolute;
+	   left: 50%;
+	   top: 50%;
+	   transform: translate(-50%,-50%);
+   }
+</style>
